fb1f9689423c4f41ca436e08906f34f8e85b79d9
Adam Spencer <aspencer0919@gmail.com>
2020-04-01 21:23:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,13 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts katz_deli[]
+    customer = []
+    counter = 1
+    katz_deli.each do |name|
+      customer.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{customer.join(" ")}"
   end
 end
 
